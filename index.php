500bdcc5f1e1fb3aaedfd2acdd81fd93ee032f57
Ilan Francisco <ilanfsilva@gmail.com>
2018-09-09 20:44:05 -0300

    Added multiple 'Acesso' POST (on /acessos)

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ $app->get('/', function (Request $request, Response $response) use ($app) {
 
 $app->post('/acesso', function (Request $request, Response $response) use ($app) {
     $params = (object) $request->getParams();
-    
+
     $entityManager = $this->get('em');
-    
+
     $acesso = (new Acesso())->setMac($params->mac)
         ->setRssi($params->rssi);
-    
+
     /**
      * Persiste a entidade no banco de dados
      */
@@ -30,12 +30,31 @@ $app->post('/acesso', function (Request $request, Response $response) use ($app)
     return $return;
 });
 
+$app->post('/acessos', function (Request $request, Response $response) use ($app) {
+    $params = $request->getParams();
+
+    $entityManager = $this->get('em');
+
+    //TODO VALIDATE received JSON on $params
+    foreach ($params as $single_param) {
+        $acesso_param = (object) $single_param;
+
+        $acesso = (new Acesso())->setMac($acesso_param->mac)->setRssi($acesso_param->rssi);
+
+        $entityManager->persist($acesso);
+        $entityManager->flush();
+    }
+
+    $return = $response->withStatus(201);
+    return $return;
+});
+
 $app->get('/acesso/{id}', function (Request $request, Response $response) use ($app) {
     $route = $request->getAttribute('route');
     $id = $route->getArgument('id');
     $entityManager = $this->get('em');
     $acessosRepository = $entityManager->getRepository('App\Models\Entity\Acesso');
-    $acesso = $acessosRepository->find($id);        
+    $acesso = $acessosRepository->find($id);
     $return = $response->withJson($acesso, 200)
         ->withHeader('Content-type', 'application/json');
     return $return;
